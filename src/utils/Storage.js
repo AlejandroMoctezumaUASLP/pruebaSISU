fd934465f8925dd1c0869c904f7983010446fa4e
AlejandroMoctezumaUASLP <moctezuma963@alumnos.ualsp.edu.mx>
2022-03-30 18:50:58 -0600

    Comentarios agregados al último commit

--- src/utils/Storage.js
+++ src/utils/Storage.js
@@ -1,6 +1,18 @@
 import { storage } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
+/**
+ * Función asíncrona que da de alta una imagen en Firebase Storage:
+ * @param {image} image La imagen a subir
+ * @return {string} URL donde se encuentra la imagen en Storage.
+ */
+
+/**************************
+ * Verifica que no sea null
+ * @param {image} image La imagen a validar
+ * @return {bolean} Determina si el password es válido o no
+ */
+
 export const subirImagen = async (imagen) => {
     const directorio = `imagenes/${imagen.name}`;
     const imagenRef = ref(storage, directorio);
